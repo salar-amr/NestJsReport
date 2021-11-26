c4c2fc89179b0494fc6c7e6cd09b6f0aebd7575c
salar-amr <salar.amirahmadiii@gmail.com>
2021-11-26 17:30:20 +0330

    initial c

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Before run a request handler
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Before response sent out
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
